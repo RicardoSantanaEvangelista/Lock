cca617c728b5798a10dd7299580797077c28b84f
Ricardo <RicardoSantanaEvangelista@gmail.com>
2021-04-24 20:25:14 -0300

    Source

---
+++ Gerador de senha/App.js
@@ -0,0 +1,136 @@
+import { StatusBar } from 'react-native';
+import React, { useState } from 'react';
+import {View,
+        Text,
+        StyleSheet,
+        Image,
+        TouchableOpacity,
+        Alert,
+       } from 'react-native'; 
+
+import Slider from '@react-native-community/slider';
+import Clipboard from 'expo-clipboard';
+
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+export default function App(){
+
+  const [password, setPassword] = useState('');
+  const [size, setSize] = useState(10);
+
+  function generatePass(){
+    let pass = '';
+    for(let i = 0, n = charset.length; i < size; i++){
+      pass += charset.charAt(Math.floor(Math.random() * n))
+    }
+
+    setPassword(pass);
+
+  }
+
+  function copyPass(){
+    Clipboard.setString(password);
+    Alert.alert('Copiado', 'Sua senha foi copiada com sucesso !');
+  }
+
+
+  return(
+    <View style={styles.container}>
+      <StatusBar
+        barStyle = "dark-content"
+        hidden = {false}
+        backgroundColor = "#0066CC"
+        translucent = {false}
+        networkActivityIndicatorVisible = {true}
+      />
+      <Image
+        source={require('./src/assets/unlock.png')}
+        style={styles.logo}
+      />
+
+      <Text style={styles.title}>{size} Caracteres</Text>
+
+      <View style={styles.area}>
+        <Slider
+          style={{height: 50}}
+          minimumValue={5}
+          maximumValue={15}
+          minimumTrackTintColor="orange"
+          maximumTrackTintColor="#000"
+          value={size}
+          onValueChange={ (valor) => setSize(valor.toFixed(0)) }
+        />
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={generatePass}>
+        <Text style={styles.buttonText}>Gerar Senha</Text>
+      </TouchableOpacity>
+
+
+
+      {password !== '' && (
+        <View style={styles.area}>
+          <Text style={styles.password} onLongPress={copyPass}> {password} </Text>
+        </View>
+      )}
+      
+     <Text style={styles.rodape}>
+          ₢Ricardo-San
+     </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F3F3FF',
+  },
+
+  logo:{
+    marginBottom: 60,
+  },  
+
+  title:{
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+
+  area:{
+    marginTop: 15,
+    marginBottom: 15,
+    backgroundColor: '#FFF',
+    width: '80%',
+    borderRadius: 7,
+  },  
+  button:{
+    backgroundColor: "#FFA200",
+    width: '80%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 7,
+    marginBottom: 25,
+  },
+
+  buttonText:{
+    fontSize: 20,
+    color: "#FFF",
+    fontWeight: 'bold',
+  },
+ 
+  password:{
+    padding: 10,
+    textAlign: 'center',
+    fontSize: 20
+  },
+  rodape:{
+    fontSize: 25,
+    marginTop: 15,
+    color: 'orange',
+    fontWeight: 'bold',
+  },
+
+});
